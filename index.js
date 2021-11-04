35a104209862aba6767f7a699b0a133378097a42
deborahlim <deborahlimhy@gmail.com>
2021-11-04 06:35:14 +0000

    impletement checkout session complete stripe webhook

--- index.js
+++ index.js
@@ -1,117 +1,117 @@
-const express = require( "express" );
-const hbs = require( "hbs" );
-const wax = require( "wax-on" );
-require( "dotenv" ).config();
-const session = require( 'express-session' );
-const flash = require( 'connect-flash' );
-const FileStore = require( 'session-file-store' )( session );
-const csrf = require( 'csurf' )
-const cors = require( 'cors' );
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+require("dotenv").config();
+const session = require("express-session");
+const flash = require("connect-flash");
+const FileStore = require("session-file-store")(session);
+const csrf = require("csurf");
+const cors = require("cors");
 // create an instance of express app
 let app = express();
 
 // static folder
-app.use( express.static( 'public' ) );
+app.use(express.static("public"));
 // set the view engine
-app.set( "view engine", "hbs" );
-
-
+app.set("view engine", "hbs");
 
 // setup wax-on
-wax.on( hbs.handlebars );
-wax.setLayoutPath( "./views/layouts" );
-
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
 
 // enable forms
 app.use(
-    express.urlencoded( {
-        extended: false
-    } )
+  express.urlencoded({
+    extended: false,
+  })
 );
 // Cors must be enabled before session
-app.use( cors() )
+app.use(cors());
 
 // set up sessions
-app.use( session( {
+app.use(
+  session({
     store: new FileStore(),
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
-    saveUninitialized: true
-} ) );
+    saveUninitialized: true,
+  })
+);
 
-app.use( flash() )
+app.use(flash());
 // Register Flash middleware
-app.use( function ( req, res, next ) {
-    res.locals.success_messages = req.flash( "success_messages" );
-    res.locals.error_messages = req.flash( "error_messages" );
-    next();
-} );
+app.use(function (req, res, next) {
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.error_messages = req.flash("error_messages");
+  next();
+});
 
 // Share the user data with hbs files
-app.use( function ( req, res, next ) {
-    res.locals.user = req.session.user;
-    next();
-} )
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  next();
+});
 // enable protection from cross site request forgery
 // app.use( csrf() );
 const csrfInstance = csrf();
-app.use( function ( req, res, next ) {
-    // exclude /checkout/process_payment for CSRF
-    if ( req.url.slice( 0, 5 ) == "/api/" ) {
-        return next()
-    }
-    csrfInstance( req, res, next )
-} )
+app.use(function (req, res, next) {
+  // exclude /checkout/process_payment for CSRF
+  if (req.url.slice(0, 5) == "/api/") {
+    return next();
+  }
+  csrfInstance(req, res, next);
+});
 
 // handle CSRF error
-app.use( function ( err, req, res, next ) {
-    if ( err && err.code == "EBADCSRFTOKEN" ) {
-        req.flash( 'error_messages', 'The form has expired. Please try again' );
-        res.redirect( 'back' );
-    } else {
-        next()
-    }
-} );
+app.use(function (err, req, res, next) {
+  if (err && err.code == "EBADCSRFTOKEN") {
+    req.flash("error_messages", "The form has expired. Please try again");
+    res.redirect("back");
+  } else {
+    next();
+  }
+});
 
 // Share CSRF with hbs files
-app.use( function ( req, res, next ) {
-    if ( req.csrfToken ) {
-        res.locals.csrfToken = req.csrfToken();
-    }
-    next();
-} )
+app.use(function (req, res, next) {
+  if (req.csrfToken) {
+    res.locals.csrfToken = req.csrfToken();
+  }
+  next();
+});
 // import in routes
-const landingRoutes = require( './routes/landing' );
-const booksRoutes = require( "./routes/books" );
-const cloudinaryRoutes = require( "./routes/cloudinary" );
-const userRoutes = require( "./routes/users" );
-const authorRoutes = require( "./routes/authors" );
-const publisherRoutes = require( "./routes/publishers" );
-const genreRoutes = require( "./routes/genres" );
-const tagRoutes = require( "./routes/tags" );
+const landingRoutes = require("./routes/landing");
+const booksRoutes = require("./routes/books");
+const cloudinaryRoutes = require("./routes/cloudinary");
+const userRoutes = require("./routes/users");
+const authorRoutes = require("./routes/authors");
+const publisherRoutes = require("./routes/publishers");
+const genreRoutes = require("./routes/genres");
+const tagRoutes = require("./routes/tags");
 const api = {
-    books: require( "./routes/api/books" ),
-    customers: require( "./routes/api/customers" ),
-    cart: require( "./routes/api/cart" ),
-    checkout: require( "./routes/api/checkout" )
-}
+  books: require("./routes/api/books"),
+  customers: require("./routes/api/customers"),
+  cart: require("./routes/api/cart"),
+  checkout: require("./routes/api/checkout"),
+};
 
 async function main() {
-    app.use( "/", landingRoutes );
-    app.use( "/books", booksRoutes );
-    app.use( "/cloudinary", cloudinaryRoutes );
-    app.use( "/users", userRoutes );
-    app.use( "/authors", authorRoutes );
-    app.use( "/publishers", publisherRoutes );
-    app.use( "/genres", genreRoutes );
-    app.use( "/tags", tagRoutes );
-    app.use( "/api/books", express.json(), api.books )
-    app.use( "/api/customers", express.json(), api.customers )
-    app.use( "/api/cart", express.json(), api.cart )
-    app.use( "/api/checkout", express.json(), api.checkout )
+  app.use("/", landingRoutes);
+  app.use("/books", booksRoutes);
+  app.use("/cloudinary", cloudinaryRoutes);
+  app.use("/users", userRoutes);
+  app.use("/authors", authorRoutes);
+  app.use("/publishers", publisherRoutes);
+  app.use("/genres", genreRoutes);
+  app.use("/tags", tagRoutes);
+  app.use("/api/books", express.json(), api.books);
+  app.use("/api/customers", express.json(), api.customers);
+  app.use("/api/cart", express.json(), api.cart);
+  app.use('/api/checkout/process_payment', express.raw({type: "*/*"}))
+  app.use("/api/checkout", express.json(), api.checkout);
 }
 main();
 
-app.listen( 3000, () => {
-    console.log( "Server has started" );
-} );
+app.listen(3000, () => {
+  console.log("Server has started");
+});
--- routes/api/checkout.js
+++ routes/api/checkout.js
@@ -1,71 +1,97 @@
-const express = require( 'express' );
-const CartServices = require( '../../services/cart_services' );
+const express = require("express");
+const CartServices = require("../../services/cart_services");
 const router = express.Router();
-const Stripe = require( 'stripe' )( process.env.STRIPE_KEY_SECRET )
-const bodyParser = require( 'body-parser' );
-const {
-    checkIfAuthenticatedJWT
-} = require( '../../middlewares' )
-router.get( '/', checkIfAuthenticatedJWT, async function ( req, res ) {
-    // in stripe -- a payment object represents one transaction
-    // a payment is defined by many line items
-    let lineItems = [];
-    let meta = [];
-    console.log( req.query.customer_id )
-    let cart = new CartServices( parseInt( req.query.customer_id ) );
-    console.log( "CART=====", cart )
-    let items = await cart.getCart();
+const Stripe = require("stripe")(process.env.STRIPE_KEY_SECRET);
+const bodyParser = require("body-parser");
+const { checkIfAuthenticatedJWT } = require("../../middlewares");
+router.get("/", checkIfAuthenticatedJWT, async function (req, res) {
+  // in stripe -- a payment object represents one transaction
+  // a payment is defined by many line items
+  let lineItems = [];
+  let meta = [];
+  let cart = new CartServices(parseInt(req.query.customer_id));
 
-    console.log( "ITEMS======", items )
-    // STEP 1 - create the line items
-    for ( let item of items ) {
-        // the keys of a line item are predefined and fixed by Stripe
-        // and all of them must be present        
-        let lineItem = {
-            'name': item.related( 'books' ).get( 'title' ),
-            'amount': item.related( 'books' ).get( 'cost' ),
-            'quantity': item.get( 'quantity' ),
-            'currency': 'SGD'
-        }
-        if ( item.related( 'books' ).get( 'imageUrl' ) ) {
-            lineItem[ 'images' ] = [ item.related( 'books' ).get( 'imageUrl' ) ]
-        }
-        // add the line item to the array of line items
-        lineItems.push( lineItem );
+  let items = await cart.getCart();
 
-        // add in the id of the books and the quantity
-        meta.push( {
-            'book_id': item.get( 'book_id' ),
-            'quantity': item.get( 'quantity' )
-        } )
+  // STEP 1 - create the line items
+  for (let item of items) {
+    // the keys of a line item are predefined and fixed by Stripe
+    // and all of them must be present
+    let lineItem = {
+      name: item.related("books").get("title"),
+      amount: item.related("books").get("cost"),
+      quantity: item.get("quantity"),
+      currency: "SGD",
+    };
+    if (item.related("books").get("imageUrl")) {
+      lineItem["images"] = [item.related("books").get("imageUrl")];
+    }
+    // add the line item to the array of line items
+    lineItems.push(lineItem);
+
+    // add in the id of the books and the quantity
+    meta.push({
+      book_id: item.get("book_id"),
+      quantity: item.get("quantity"),
+    });
+  }
+
+  // STEP 2 - create stripe payment object`
+
+  // convert our meta data into a JSON string
+  let metaData = JSON.stringify(meta);
+  // the keys of the payment object are fixed by stripe
+  // all of them are complusory (except metadata)
+  const payment = {
+    payment_method_types: ["card"],
+    line_items: lineItems,
+    success_url: process.env.STRIPE_SUCCESS_URL,
+    cancel_url: process.env.STRIPE_CANCEL_URL,
+    metadata: {
+      orders: metaData,
+      customer_id: req.query.customer_id,
+    },
+  };
+
+
+  // create the payment session with the payment object
+  let stripeSession = await Stripe.checkout.sessions.create(payment);
+  res.send({
+    sessionId: stripeSession.id,
+    publishableKey: process.env.STRIPE_KEY_PUBLISHABLE,
+  });
+});
+
+// webhook which Stripe will call to process the payment
+router.post('/process_payment', express.raw({"type":"application/json"}), function(req,res){
+    // req contains data send to this endpoint from Stripe
+    // and is only sent when Stripe completes a payment
+    let payload = req.body;
+    console.log("PAYLOAD", payload)
+    // we need an endpointSecret to verify that this request is actually sent from stripes
+    let endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+    console.log("ENDPOINT SECRET= ", endpointSecret)
+    let sigHeader = req.headers['stripe-signature'];
+    console.log("SIG HEADER= ", sigHeader);
+    let event;
+    try {
+        event = Stripe.webhooks.constructEvent(payload, sigHeader, endpointSecret);
+        console.log("EVENT", event);
+    } catch(e) {
+        // the stripe request is invalid (i.e not from stripe)
+        res.send({
+            'error':e.message
+        })
+        console.log(e.message);
+    }
+    // if the stripe request is verified to be from stripe
+    // then we recreate payment session
+    let stripeSession = event.data.object;
+    if (event.type == 'checkout.session.completed') {
+        console.log(stripeSession);
     }
 
-    // STEP 2 - create stripe payment object`
+    res.send({'recieved': true})
+})
 
-    // convert our meta data into a JSON string
-    let metaData = JSON.stringify( meta );
-    // the keys of the payment object are fixed by stripe
-    // all of them are complusory (except metadata)
-    console.log( "METADATA==============", metaData )
-    const payment = {
-        'payment_method_types': [ 'card' ],
-        'line_items': lineItems,
-        'success_url': process.env.STRIPE_SUCCESS_URL,
-        'cancel_url': process.env.STRIPE_CANCEL_URL,
-        'metadata': {
-            'orders': metaData,
-            'customer_id': req.query.customer_id
-        }
-    }
-    console.log( "PAYMENT", payment )
-
-    // create the payment session with the payment object
-    let stripeSession = await Stripe.checkout.sessions.create( payment );
-    console.log( "SESSION_ID============", stripeSession )
-    res.send( {
-        'sessionId': stripeSession.id,
-        'publishableKey': process.env.STRIPE_KEY_PUBLISHABLE
-    } )
-} )
-
-module.exports = router
+module.exports = router;
