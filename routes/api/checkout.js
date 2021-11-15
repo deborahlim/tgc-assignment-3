6a86fd4b24d92f461a4e888ff27b403e07219c98
deborahlim <deborahlimhy@gmail.com>
2021-11-15 12:40:36 +0000

    only create line items if cart is not empty

--- routes/api/checkout.js
+++ routes/api/checkout.js
@@ -8,6 +8,7 @@ const {
 const bookDataLayer = require("../../dal/books");
 const orderDataLayer = require("../../dal/orders");
 router.get("/", checkIfAuthenticatedJWT, async function (req, res) {
+
   // in stripe -- a payment object represents one transaction
   // a payment is defined by many line items
   let lineItems = [];
@@ -15,54 +16,55 @@ router.get("/", checkIfAuthenticatedJWT, async function (req, res) {
   let cart = new CartServices(parseInt(req.query.customer_id));
 
   let items = await cart.getCart();
+  if (items.length !== 0) {
+    // STEP 1 - create the line items
+    for (let item of items) {
+      // the keys of a line item are predefined and fixed by Stripe
+      // and all of them must be present
+      let lineItem = {
+        name: item.related("books").get("title"),
+        amount: item.related("books").get("cost"),
+        quantity: item.get("quantity"),
+        currency: "SGD",
+      };
+      if (item.related("books").get("imageUrl")) {
+        lineItem["images"] = [item.related("books").get("imageUrl")];
+      }
+      // add the line item to the array of line items
+      lineItems.push(lineItem);
 
-  // STEP 1 - create the line items
-  for (let item of items) {
-    // the keys of a line item are predefined and fixed by Stripe
-    // and all of them must be present
-    let lineItem = {
-      name: item.related("books").get("title"),
-      amount: item.related("books").get("cost"),
-      quantity: item.get("quantity"),
-      currency: "SGD",
-    };
-    if (item.related("books").get("imageUrl")) {
-      lineItem["images"] = [item.related("books").get("imageUrl")];
+      // add in the id of the books and the quantity
+      meta.push({
+        book_id: item.get("book_id"),
+        quantity: item.get("quantity"),
+      });
     }
-    // add the line item to the array of line items
-    lineItems.push(lineItem);
 
-    // add in the id of the books and the quantity
-    meta.push({
-      book_id: item.get("book_id"),
-      quantity: item.get("quantity"),
+    // STEP 2 - create stripe payment object`
+
+    // convert our meta data into a JSON string
+    let metaData = JSON.stringify(meta);
+    // the keys of the payment object are fixed by stripe
+    // all of them are complusory (except metadata)
+    const payment = {
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+      metadata: {
+        orders: metaData,
+        customer_id: req.query.customer_id,
+      },
+    };
+
+
+    // create the payment session with the payment object
+    let stripeSession = await Stripe.checkout.sessions.create(payment);
+    res.send({
+      sessionId: stripeSession.id,
+      publishableKey: process.env.STRIPE_KEY_PUBLISHABLE,
     });
   }
-
-  // STEP 2 - create stripe payment object`
-
-  // convert our meta data into a JSON string
-  let metaData = JSON.stringify(meta);
-  // the keys of the payment object are fixed by stripe
-  // all of them are complusory (except metadata)
-  const payment = {
-    payment_method_types: ["card"],
-    line_items: lineItems,
-    success_url: process.env.STRIPE_SUCCESS_URL,
-    cancel_url: process.env.STRIPE_CANCEL_URL,
-    metadata: {
-      orders: metaData,
-      customer_id: req.query.customer_id,
-    },
-  };
-
-
-  // create the payment session with the payment object
-  let stripeSession = await Stripe.checkout.sessions.create(payment);
-  res.send({
-    sessionId: stripeSession.id,
-    publishableKey: process.env.STRIPE_KEY_PUBLISHABLE,
-  });
 });
 
 
@@ -127,12 +129,12 @@ router.post('/process_payment', express.raw({
       process_checkout(session);
       break;
     case 'checkout.session.expired':
-      session = event.data.object;
-      console.log("STRIPE SESSION = ", session);
-      process_checkout(session);
+      const expiredSession = event.data.object;
+      console.log("STRIPE SESSION = ", expiredSession);
+      process_checkout(expiredSession);
       break;
     case 'payment_intent.canceled':
-      payment = event.data.object;
+      const payment = event.data.object;
       console.log("PAYMENT", payment);
       break;
     default:
